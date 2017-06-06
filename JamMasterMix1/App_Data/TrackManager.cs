2f0b9d82d43981178783fc332b53d4779ad909ff
JamMasterMax <maximillian.munguia@gmail.com>
2017-06-05 23:36:28 -0700

    being reading csv

--- JamMasterMix1/App_Data/TrackManager.cs
+++ JamMasterMix1/App_Data/TrackManager.cs
@@ -11,8 +11,20 @@ namespace JamMasterMax1.App_Data
         {
             List<Track> tracks = new List<Track>();
 
-            string repositoryDirectory = System.Configuration.AppSettingsReader.AppSettings["PFUserName"];
-            var tracks = File.ReadLines(Path.Combine())
+            //string repositoryDirectory = System.Configuration.AppSettingsReader.AppSettings["PFUserName"];
+            IEnumerable<string> trackRepo = File.ReadLines("/Users/jmmax/Projects/JMM_Repositories/TrackRepository_1704.csv");
+            foreach(string trackLine in trackRepo){
+                string[] trackData = trackLine.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Track track = new Track();
+                track.TrackID = Guid.Parse(trackData[0]);
+                track.TrackName = trackData[1];
+                track.TrackArtistID = Guid.Parse(trackData[2]);
+                track.TrackAlbumID = Guid.Parse(trackData[3]);
+
+                tracks.Add(track);
+            }
+
 
             return tracks;
         }
